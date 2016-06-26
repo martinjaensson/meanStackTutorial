e18bea1790705e90fdb3ecc888c4bc42f64dd5ea
martinjaensson <martinjaensson@gmail.com>
2016-06-26 22:03:06 +0200

    mongo DB added, add and remove functionality added

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
-
-person1 = {
-	name: "Martin",
-	email: "martinjaensson@gmail.com",
-	number: "0736731222"
-};
-
-person2 = {
-	name: "Linn",
-	email: "linnalvin@gmail.com",
-	number: "073555555"
-};
-
-person3 = {
-	name: "Omed",
-	email: "omedsaeid@gmail.com",
-	number: "073666666"
-};
-
-var contactlist = [person1, person2, person3];
-
+app.use(bodyParser.json());
 
 app.get('/contactlist', function(req, res) {
 	console.log("I received GET request");
-	res.json(contactlist);
+	db.contactlist.find(function(err, docs) {
+		console.log(docs);
+		res.json(docs);
+	});
 });
+
+app.post('/contactlist', function(req,res) {
+	console.log(req.body);
+	db.contactlist.insert(req.body, function(err, doc) {
+		res.json(doc);
+	});
+});
+
+app.delete('/contactlist/:id', function(req, res) {
+	var id = req.params.id;
+	console.log(id);
+	db.contactlist.remove({_id:mongojs.ObjectId(id)}, function(err, doc) {
+		res.json(doc);
+	});
+})
+
 
 app.listen(3000);
 
--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,9 +2,27 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
 	console.log("Hello World from controller");
 	
-	$http.get('/contactlist').success(function(response) {
-		console.log('I got the data I requested');
-		$scope.contacts = response;
-	});
+	var refresh = function() {
+		$http.get('/contactlist').success(function(response) {
+			console.log('I got the data I requested');
+			$scope.contacts = response;
+			$scope.contact = "";
+		});
+	};
+	refresh();
+	$scope.addContact = function(){
+		console.log($scope.contact);
+		$http.post('/contactlist', $scope.contact).success(function(response){
+			console.log(response);
+			refresh();
+		});
+	};
 
+	$scope.removeContact = function(id) {
+		console.log(id);
+		$http.delete('/contactlist/' + id).success(function(response) {
+			refresh();
+		});
+
+	}
 }]);
