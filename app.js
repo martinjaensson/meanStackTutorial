53f7c4cc8ec104d9f9def5fb8801d9226f8c74e5
martinjaensson <martinjaensson@gmail.com>
2016-06-26 20:28:02 +0200

    Angular added, Bootstrap added, controller addeed, testing data

--- app.js
+++ app.js
@@ -3,6 +3,32 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+person1 = {
+	name: "Martin",
+	email: "martinjaensson@gmail.com",
+	number: "0736731222"
+};
+
+person2 = {
+	name: "Linn",
+	email: "linnalvin@gmail.com",
+	number: "073555555"
+};
+
+person3 = {
+	name: "Omed",
+	email: "omedsaeid@gmail.com",
+	number: "073666666"
+};
+
+var contactlist = [person1, person2, person3];
+
+
+app.get('/contactlist', function(req, res) {
+	console.log("I received GET request");
+	res.json(contactlist);
+});
+
 app.listen(3000);
 
 console.log("Server running on port 3000");
